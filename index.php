4e6dc854350780528d83efb519d159473188d3ae
Arturo-MR <arturo10.mr@gmail.com>
2014-03-31 22:07:56 -0600

    index.php
    Estructura

---
+++ index.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Casas</title>
+</head>
+<body>
+	<header>
+		<nav id="menu">
+			<ul>
+				<li>
+					<figure>
+						<img src="images/logo.png"/>
+					</figure>	
+				</li>
+
+				<li>
+					<h1>Pulse</h1>
+				</li>
+
+				<li>
+					<figure id="user">
+						<img src="images/user.jpg" alt="Admin">
+					</figure>
+				</li>
+
+				<li>
+					Inicio
+				</li>
+
+				<li>
+					Noticias
+				</li>
+
+				<li>
+					Propiedades
+				</li>
+
+				<li>
+					Contacto
+				</li>
+			</ul>
+
+		</nav>
+	</header>
+
+<!-- 	<div id="buscador">
+
+	</div>
+	<div id="slider">
+		
+	</div> -->
+	<section id="hot_item">
+		<nav id="menu_item">
+			<ul>
+				<li>DF</li>
+				<li>Querétaro</li>
+				<li>Monterrey</li>
+				<li>Pachuca</li>
+				<li>Edo.Mex</li>
+			</ul>
+		</nav>
+		<article>
+			<h2 class="titulo_item">
+				<a href="#">Título casa</a>
+			</h2>
+			<p class="precio_item">
+				$500,000
+			</p>
+		</article>
+	</section>
+	<footer>
+		<p>
+			<strong>Pulse 2014</strong>
+		</p>
+	</footer>
+</body>
+</html>

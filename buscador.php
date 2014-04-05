d2a549ea514e975bf66c2eea5a2606eec260f45a
Arturo-MR <arturo10.mr@gmail.com>
2014-04-04 18:44:08 -0600

    Se modula en buscador.php

--- buscador.php
+++ buscador.php
@@ -1,4 +1,8 @@
-<div id="buscador">
-	
+<from action="#" method="post">
+		<div id="buscador">
+			<label for="fbuscar" id="fbuscar">Buscar</label>
+			<input name="buscar" type="text" id="buscar" required>
+			<a onclick="javascript:getElementById('formulario').submit();" href="#"><img src="images/search.png" alt="submit" /></a>
 
-</div>
+		</div>
+	</form>
--- index.php
+++ index.php
@@ -12,14 +12,10 @@
 	<!-- header -->
 	<?php include ("header.php"); ?>
 	<!-- header -->
-	<from action="#" method="post">
-		<div id="buscador">
-			<label for="fbuscar" id="fbuscar">Buscar</label>
-			<input name="buscar" type="text" id="buscar" required>
-			<a onclick="javascript:getElementById('formulario').submit();" href="#"><img src="images/search.png" alt="submit" /></a>
 
-		</div>
-	</form>
+	<!-- buscador -->
+	<?php include ("buscador.php"); ?>
+	<!-- buscador -->
 <!-- 	<div id="slider">
 		
 	</div>

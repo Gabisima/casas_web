33280898fb918ed6ddb455c7fba7223f86553958
Gabisima <urbe.gabs@gmail.com>
2014-04-08 14:08:06 -0500

    Pues ya debe de servir con estilos y todo

--- catalogo.php
+++ catalogo.php
@@ -1,52 +1,53 @@
-<!doctype html>
-<html>
 <?php
 		//Conectar a la base de datos
 		include "conexion.php"; 
 		mysql_connect("localhost", "root", "") or die ("No hay nada");
 		mysql_select_db("bienesraices") or die ("No hay base");
  ?>
-<head>
 	<meta charset="utf-8">
 	<meta name="description" content="El hogar de los inmuebles">
 	<title>Pulse | Inmuebles</title>
 	<link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" href="css/estilos.css">
-</head>
 
-<body>
 	<!-- header -->
 	<?php include ("header.php"); ?>
 	<!-- buscador -->
 	<?php include ("buscador.php"); ?>
+	<!-- slider -->
+	<!--?php //include ("slider.php"); ?-->	
 
 	<h1> CATALOGO DE PROPIEDADES </h1>
-	<!-- cada una de las propiedades-->
-	<table>
-	  <thead>
-	  	<tr>
-	  		<th>Propiedad</th>
-      		<th>Descripción</th>
-      		<th>Ubicación</th>
-      		<th>Superficie</th>
-		 </tr>
-	  </thead>
 	<?php 
-		mysql_connect("localhost", "root","") or die ("Error al conectar a la base de datos.");
-  		mysql_select_db("inmuebles") or die ("Error al conectar a la base de datos.");
-
-
   	$c = @$_POST['buscar'];
   	$sql = @mysql_query("SELECT * FROM inmuebles");
 	while ($row = mysql_fetch_object($sql))
 	{
 	?>
-	  <tr>
-	    <td><img src="images/<?php echo $row->foto1; ?>" height="50" width="50"></td>
-	    <td><?php echo $row->descripcion;?></td>
-	    <td><?php echo $row->ubicacion;?></td>
-        <td><?php echo $row->superficie;?></td>
-  	  </tr>
+<section id="contenido">
+		<article class="item">
+			<figure class="imagen_item">
+				<img src="images/inmuebles/<?php echo $row->foto1; ?>">
+			</figure>
+			<h2 class="titulo_item">
+				<a href="#">
+					<?php echo $row->nombre;?>
+				</a>
+			</h2>
+
+			<div class="estado_item">
+				<?php echo $row->localidad;?>
+			</div>
+			
+			<div class="precio_item">
+				$ <?php echo $row->precio;?> MXN
+			</div>
+
+			<div class="datos_item">
+				<a href="#" class="tipo"><?php echo $row->tipo;?></a>
+			</div>
+
+		</article> 		
 <?php 	} ?>
 </table>
 <footer>
@@ -54,6 +55,3 @@
 			<strong>Pulse 2014</strong>
 		</p>
 </footer>
-</body>
-
-</html>

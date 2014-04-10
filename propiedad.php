0c6d07903182f6e0afab27072b1a41e85689a180
Arturo-MR <arturo10.mr@gmail.com>
2014-04-10 08:48:12 -0500

    Maps tamaño ajustable

--- propiedad.php
+++ propiedad.php
@@ -106,7 +106,7 @@
 			</div>
 			<div class="mapa">
 				<iframe
-			  width="600"
+			  width="100%"
 			  height="450"
 			  frameborder="0" style="border:0"
 			  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAX7C701RDmDK_RA2DfNu44-LhqnQ6zqzk&q=<?php echo $row->direccion; ?>">

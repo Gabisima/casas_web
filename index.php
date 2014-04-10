c0a0e2cd24fc9fd9ca3da9724bf2b0086a584bb1
Arturo-MR <arturo10.mr@gmail.com>
2014-04-10 09:07:14 -0500

    Direcciones de img

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
 		{ ?>
 			<?php if ($i<=6) { ?>
           
-	  			<div class="item"><a href="propiedad.php?id=<?php echo $row->id;?>"><img src="images/inmuebles/<?php echo $row->foto1;?>" alt=""></a></div>
+	  			<div class="item"><a href="propiedad.php?id=<?php echo $row->id;?>"><img src="admin/uploads/<?php echo $row->foto1;?>" alt=""></a></div>
 	  		<?php } ?>	
 	
 	<?php } ?>
@@ -101,7 +101,7 @@
 		?>
 		<article class="item">
 			<figure class="imagen_item">
-				<a href="propiedad.php?id=<?php echo $row->id;?>"><img src="images/inmuebles/<?php echo $row->foto1;?>" /></a>
+				<a href="propiedad.php?id=<?php echo $row->id;?>"><img src="admin/uploads/<?php echo $row->foto1;?>" /></a>
 			</figure>
 			<h2 class="titulo_item">
 				<a href="propiedad.php?id=<?php echo $row->id;?>">
